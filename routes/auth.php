35ac51ab7bb9c564437b6d74a5be1ddfda8b9cdd
Aws12505 <awsaljaramani12505@gmail.com>
2025-10-27 07:03:05 -0400

    ..

--- routes/auth.php
+++ routes/auth.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
-Route::get('/login',[AuthenticatedSessionController::class, 'create'])->middleware(['guest']);
+Route::get('/login',[AuthenticatedSessionController::class, 'create'])->middleware(['guest'])->name('login');
 Route::post('/login',[AuthenticatedSessionController::class, 'store'])->middleware(['guest']);
 Route::post('/logout',[AuthenticatedSessionController::class, 'destroy'])->name('logout');
